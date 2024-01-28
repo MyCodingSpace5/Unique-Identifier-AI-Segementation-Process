49607252bfe88921e551dc03e9a603c245d70681
Ahmet <121121504+MyCodingSpace5@users.noreply.github.com>
2024-01-28 14:02:38 -0500

    Create m.c

---
+++ m.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int size = 0; //Change this based on data
+
+
+struct data{
+   int size;
+   char[size] data;
+}
+
+data[size] dataset;
+
+
+
+int identifier(struct data size){
+  int sum;
+  int dataanalyzed;
+  for(int i = 0; dataset[i].data = dataset[i].data + 'm'; i++){
+    dataanalyzed++;
+    for(int z = 0; dataset[i].data[z] = dataset[i].data[z]; z++){
+        if(dataset[i].data[z] == size.data[z]){
+            z++;
+        }
+      else{
+         sum + z;
+         break;
+    }
+}
+  }
+  return sum/datanalyzed;
+}
